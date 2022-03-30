e5e7f649516e01b09f42a593f1a6e61d6b5fe8c2
minsunki <minsunki@student.42seoul.kr>
2022-03-30 16:13:11 +0900

    ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 16:07:01 by minsunki          #+#    #+#             */
+/*   Updated: 2022/03/30 16:09:53 by minsunki         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __WHATEVER_HPP__
+#define __WHATEVER_HPP__
+
+template <class T>
+void	swap(T &a, T &b);
+
+template <class T>
+T	&min(T &a, T &b);
+
+template <class T>
+T	&max(T &a, T &b);
+
+#endif
